78c0077c5fd5680b605da2ddd24e4849599dd960
sd3227 <55206090+sd3227@users.noreply.github.com>
2019-09-19 08:03:44 +0800

    Create Sen Dai sd3227 Project1.m

---
+++ doc/Sen Dai sd3227 Project1.m
@@ -0,0 +1,12 @@
+$ git config --global core.autocrlf input
+
+# Sen Dai sd3227 Project1
+Nowadays, everyone loves to listen to music. They not only love the melody, but also love the lyrics. However, different genres of music have different type of lyrics. The interesting thing is, that we can get lots of hints from analyzing the lyrics instead of listening the music. To get the secret of the lyrics, I decide to analysis some lyrics of different genres from 1970s to 2010s by using R. I only foucs on three aspect:
+    1.What's the popular genres of music in different decades? 
+    2.What's the difference of lyrics in different genres of music? 
+    3.What's the habit of the artist to write the lyrics?Are they Shakespeare or Dumas?
+    I analysis the data by different methods and get lots of interesting results.I'll show what i did step by step and tell a fantastic "data story".  
+Firstly, I load all the required libraries.
+Secondly, I load and clean the data for future use.
+ Rightnow,I get a question. I really want to know what kind of music do people love at differet decades. To solve this question, I plot some graphs to get the answer. 
+    From Fig1.1, we can see that different type of music at different decades.It's difficult to know what kind of music do people like from 1970s to 1990s since the data is not enough. We can easily find out Rock is the most popular music in 2000s, the second popular one is Pop and the third one is Metal. However, in 2010s, the third popular changed from Metal to Hip Hop. Hence we can see that with the time, listener change their preference, but not a lot. 
